d1004f4987648982ae5e1051c2847fe374b9e3f9
SChoolWab <schoolwab@gmail.com>
2019-09-03 19:07:40 +0600

    Updates

---
+++ precache-manifest.ee750a380e28ab74bc15bdcc290c3ee2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c93a5c33353774960a0422665018c6b7",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "77fb97d824b3594432fe",
+    "url": "/website/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "da8f8f64c83d749d8242",
+    "url": "/website/static/css/main.a5cbf705.chunk.css"
+  },
+  {
+    "revision": "77fb97d824b3594432fe",
+    "url": "/website/static/js/2.9b2aef01.chunk.js"
+  },
+  {
+    "revision": "da8f8f64c83d749d8242",
+    "url": "/website/static/js/main.3f7588b6.chunk.js"
+  },
+  {
+    "revision": "222ad48f7de41c8e84cc",
+    "url": "/website/static/js/runtime~main.dcc8089f.js"
+  },
+  {
+    "revision": "988f3419ee580191a3c5ab49664a341b",
+    "url": "/website/static/media/SIYAMRUPALI.988f3419.TTF"
+  },
+  {
+    "revision": "7051bc058240044c49587404e419bca9",
+    "url": "/website/static/media/kalpurush.7051bc05.ttf"
+  }
+]);
